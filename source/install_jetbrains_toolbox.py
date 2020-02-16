f67f2d98029d995009538bf01d3055300d4fa70a
Bent <brundtoe@outlook.dk>
2020-02-16 18:36:15 +0100

    utility som downloader og installerer jetbrains-toolbox

---
+++ source/install_jetbrains_toolbox.py
@@ -0,0 +1,24 @@
+# installation af Google Chrome
+
+import sys,os, shutil
+from moduler.fileOperations import download_file, fetch_config
+
+try:
+    configs = fetch_config('../config/config.ini')
+    url = configs['jetbrains.toolbox']['url']
+    version = configs['Common']['jetbrains-toolbox']
+    user = configs['Common']['user']
+    print(url)
+    outfile = download_file(url)
+    shutil.unpack_archive(outfile,f'/home/bin/{user}','gztar')
+    # outdir = f'/home/{user}/Downloads/jetbrains-toolbox-{version}'
+    # src = f'/jetbrains-toolbox{outdir}'
+    # dst = f'/home/bin/{user}/jetbrains-toolbox'
+    # shutil.copyfile(src,dst)
+    # os.remove(outdir)
+except Exception as err:
+    sys.exit('Download af jetbrains toolbox er fejlet\n',err)
+else:
+    print('JetBrains toolbox er downloded og pakket ud')
+
+
