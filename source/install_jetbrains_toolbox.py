420c1968d9a351ad3c60e86e0634c014dee475bc
Bent <brundtoe@outlook.dk>
2020-02-17 09:29:47 +0100

    Rettet fejl i angivelse af destination library

--- source/install_jetbrains_toolbox.py
+++ source/install_jetbrains_toolbox.py
@@ -10,10 +10,10 @@ try:
     user = configs['Common']['user']
     print(url)
     outfile = download_file(url)
-    shutil.unpack_archive(outfile,f'/home/bin/{user}','gztar')
+    shutil.unpack_archive(outfile,f'/home/{user}/bin/','gztar')
     # outdir = f'/home/{user}/Downloads/jetbrains-toolbox-{version}'
     # src = f'/jetbrains-toolbox{outdir}'
-    # dst = f'/home/bin/{user}/jetbrains-toolbox'
+    # dst = f'/home/{user}/bin/jetbrains-toolbox'
     # shutil.copyfile(src,dst)
     # os.remove(outdir)
 except Exception as err:
