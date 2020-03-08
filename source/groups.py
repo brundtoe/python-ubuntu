678415a41b3476fafa73f64675d51eccb1ed9bff
Jackie <brundtoe@outlook.dk>
2020-03-08 14:23:26 +0000

    funktionen adduser udgået da kommandoen ikke findes på Manjaro

--- source/groups.py
+++ source/groups.py
@@ -4,24 +4,6 @@ import sys, os, shlex, subprocess
 from moduler.fileOperations import fetch_config
 
 
-def adduser(user, group):
-    """
-    add a user to a group
-    :param user: user name
-    :param group: group name
-    :return:
-    """
-    cmd = shlex.split(f'adduser {user} {group}')
-    try:
-        res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if res.returncode:
-            raise Exception
-    except Exception as err:
-        print()
-        sys.exit(res.stderr.decode('UTF-8'))
-    print(res.stdout.decode('UTF-8'))
-
-
 def usermod(user, group, options=''):
     """
     Add a user to a group using usermod
@@ -31,6 +13,7 @@ def usermod(user, group, options=''):
     :return:
     """
     cmd = shlex.split(f'usermod {options} -G {group} -a {user}')
+    res = ''
     try:
         res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if res.returncode:
@@ -46,8 +29,5 @@ if __name__ == '__main__':
         sys.exit('scriptet skal udføres med root adgang')
 
     user = fetch_config('../config/config.ini')['Common']['user']
-    group = 'docker'
-    ##add user to existing group
-    adduser(user, 'vboxusers')
-    # modificer user 
-    usermod(user, group)
+    usermod(user, 'docker')
+    usermod(user, 'vboxusers')
