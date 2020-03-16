3e897e1826eb4c56c414a1d0ab156b0626d0852a
Jackie <brundtoe@outlook.dk>
2020-03-16 15:50:24 +0100

    installation på Kubuntu manglede

--- source/install_smartgit.py
+++ source/install_smartgit.py
@@ -2,5 +2,28 @@
 #
 # Installation af smartgit
 #
+import sys, os
+from moduler.fileOperations import fetch_config, fetch_archive
+from moduler.desktopfile import create_desktop_file
 
-print('På Debian like distributioner tilgås SmartGit via StartMenuen')
+def install_smartgit(configs):
+    """
+    Installation af SmartGit
+    Forudsætter installation af gtk2
+    :param configs: parametre fra config.ini
+    :return: void
+    """
+
+    url = configs['syntevo.com']['url_targz']
+    user = configs['Common']['user']
+    program = 'SmartGit'
+    version = configs['Common']['smartgit']
+    fetch_archive(url, user, program, version)
+
+
+    tmpl = f'{program}.jinja'
+    create_desktop_file(program, tmpl, user)
+
+if __name__ == '__main__':
+    configs = fetch_config('../config/config.ini')
+    install_smartgit(configs)
