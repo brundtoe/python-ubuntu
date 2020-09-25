48ac321fe547d15810a56081bebccc9c4ecf35a4
Jackie <brundtoe@outlook.dk>
2020-09-25 14:52:57 +0200

    version 2 af extra diske og wdmycloud nu mod /etc/fstab

--- source/wdmycloud.py
+++ source/wdmycloud.py
@@ -4,30 +4,19 @@
 import sys, os, shlex
 import subprocess
 
-
-def disk_exists(disk_entry):
-    disk_path = disk_entry[0:54]
-    if os.path.exists(disk_path):
-        # print(disk_entry)
-        return True
-    else:
-        print(f'Oops disken {disk_path} findes IKKE')
-        return False
-
-
 def add_line(filename, disk_entry):
-    disk = disk_entry[0:54]
+    disk = disk_entry.split()
     # print(f'Tilføj .. {disk}')
     try:
-        cmd = shlex.split(f'grep -qF "{disk}" {filename}')
+        cmd = shlex.split(f'grep -qF "{disk[0]}" {filename}')
         res = subprocess.run(cmd)
         if res.returncode != 0:
-            #print('så opdateres')
+            # print('så opdateres')
             cmd = shlex.split(f'sed -i "$a {disk_entry}" {filename}')
             res = subprocess.run(cmd)
-            #print(res)
+            # print(res)
         else:
-            print(f'Mount {disk} ... findes allerede i fstab')
+            print(f'Mount {disk[0]} ... findes allerede i fstab')
     except Exception as err:
         print(err)
         sys.exit(f'kan ikke opdatere fstab for disken {disk}')
@@ -36,10 +25,7 @@ def add_line(filename, disk_entry):
 def update_fstab(filename, filename_disks):
     with open(filename_disks) as src_file:
         for line in src_file:
-            if disk_exists(line):
-                add_line(filename, line)
-            else:
-                print(f'Disken {line[0:54]}findes ikke')
+            add_line(filename, line)
 
 
 if __name__ == '__main__':
@@ -47,7 +33,6 @@ if __name__ == '__main__':
         sys.exit('Scriptet skal udføres  med root access')
     print('Konfiguration af ekstra diske')
     # configs = fetch_config('../config/config.ini')
-    filename_disks = '../config/extradiske'
-    #filename = '/home/jackie/Downloads/untitled'
+    filename_disks = '../config/wdmycloud'
     filename = '/etc/fstab'
     update_fstab(filename, filename_disks)
