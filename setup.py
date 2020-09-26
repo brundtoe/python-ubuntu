f1d6ee9b11fad2d46c3fff516c0ab3f6efc24818
Jackie <brundtoe@outlook.dk>
2020-09-26 18:23:19 +0200

    eksperiment med at builde appen med setup tools til et python egg

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-ubuntu',
+    packages=find_packages(),
+)
---
+++ source/start.py
@@ -0,0 +1,7 @@
+import sys
+from lib import jensen
+
+if __name__ == '__main__':
+    print('hello from start')
+    jensen.jensen_print()
+    print(sys.path)
