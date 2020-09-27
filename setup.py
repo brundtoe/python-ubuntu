692ef9823f7244005f7782100f2250b6832e3fed
Jackie <brundtoe@outlook.dk>
2020-09-27 10:55:23 +0200

    opdateret setup med yderligere metatada

--- setup.py
+++ setup.py
@@ -2,5 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-ubuntu',
+    description='Installation på Kubuntu og Manjaro',
+    version='0.1.0',
+    license='MIT',
+    author='Jackie B',
+    author_email='brundtoe@outlook.dk',
+    install_requires=[
+        'requests == 2.24.0',
+        'Jinja2 == 2.11.2',
+        'Sphinx == 3.2.1',
+        'sphinx-rtd-theme == 0.5.0'
+    ],
     packages=find_packages(),
 )
