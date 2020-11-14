e9270c59a3e36d44039fc7b808e140c08bd3de26
jackie <brundtoe@outlook.dk>
2020-11-14 15:42:22 +0100

    tilføejt php config

--- devops/bento_scripts/install_php.sh
+++ devops/bento_scripts/install_php.sh
@@ -34,4 +34,59 @@ apt-get install -y \
    php7.4-zip \
    php-xdebug
 
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Add Composer Global Bin To Path
+printf "\nPATH=\"$(sudo su - vagrant -c 'composer config -g home 2>/dev/null')/vendor/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
+
+# set php cli settings in php.ini
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/cli/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/cli/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.4/cli/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.4/cli/php.ini
+
+# set php fpm settings in php.ini
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.4/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.4/fpm/php.ini
+
+# configure  xdebug options for php-fpm
+
+echo "xdebug.remote_enable = 1" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "xdebug.max_nesting_level = 512" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "opcache.revalidate_freq = 0" >> /etc/php/7.4/mods-available/opcache.ini
+
 echo "Det var installationen af php"
+
+# install nginx
+
+apt-get install -y nginx
+
+# Set The Nginx & PHP-FPM User
+sed -i "s/user www-data;/user vagrant;/" /etc/nginx/nginx.conf
+sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
+
+sed -i "s/user = www-data/user = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
+
+sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.4/fpm/pool.d/www.conf
+
+service nginx restart
+servicce php7.4-fpm restart
+
+# Add Vagrant User To WWW-Data
+usermod -a -G www-data vagrant
+id vagrant
+groups vagrant
