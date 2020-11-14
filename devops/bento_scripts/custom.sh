9bea6f268d71b84c7626420ee65eccdadba479b5
jackie <brundtoe@outlook.dk>
2020-11-14 11:40:10 +0100

    opdatere installationa f den basae software pakker

--- devops/bento_scripts/custom.sh
+++ devops/bento_scripts/custom.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 
+set -eux
+export DEBIAN_FRONTEND=noninteractive
+
 #mine tilføjelser til bento/ubuntu boxes
 
-# Installer pakkerne
-PACKAGES="gcc make
-linux-headers-$(uname -r) build-essential dkms
-vim curl wget git lynx
-cifs-utils samba
-sqlite3 libsqlite3-dev
-python2.7 python-dev python2.7-dev
-unixodbc unixodbc-dev
-libssl-dev libmysqlclient-dev
-patch ruby-dev
-zlib1g-dev liblzma-dev libkrb5-dev libkrb5-dbg
-"
-# PHP7.2 må ikke installeres, da den også installerer Apache2
-# Det er ikke nødvendigt at installere php7.2-cli og php7.2-cgi
+sudo apt-get update
+sudo apt-get upgrade -y
+# dpkg skal være installeret, da den anvendes senere i scriptet
+sudo apt-get -y install dpkg
 
-for package in $PACKAGES
-do
-  apt-get install -y $package
-done
-
-bindir=/home/vagrant/bin
-if ! [ -d $bindir ]
-then
-   printf "Opretter /home/vagrant/bin"
-   sudo mkdir $bindir && sudo chown vagrant:vagrant $bindir
-fi
+sudo apt-get install -y \
+  lsb-release \
+  dkms \
+  build-essential \
+  software-properties-common \
+  linux-headers-generic \
+  apt-transport-https \
+  gnupg \
+  vim \
+  curl \
+  wget \
+  git \
+  cifs-utils \
+  python3-pip \
+  python3-setuptools \
+  python-apt \
+  python3-virtualenv \
+  sqlite3 \
+  libsqlite3-dev
