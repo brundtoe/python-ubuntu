cd91dd51d2e00a916a437655d110f85d343b611d
Jackie <brundtoe@outlook.dk>
2020-11-15 17:26:52 +0100

    fjernet resterne af det gamle devops projekt. scripts og cnf filer er implementeret i va-devops/valdemar

--- devops/devops-files/sambapassword.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-PASS=vagrant
-USER=vagrant
-echo -ne "$PASS\n$PASS\n" | smbpasswd -a -s $USER
--- devops/scripts/scrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash 
-
-set -eu
-
-if [ $(whoami) != "root" ]; then
-        echo "Script must be run as user: root"
-        exit -1
-fi
-
