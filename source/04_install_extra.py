6c4f0abdea8bae197d9dbafe877fb447bc647ef8
jackie <brundtoe@outlook.dk>
2020-12-17 17:05:28 +0100

    opdateret installation af docker og docker-compose til seneste udgave

--- source/04_install_extra.py
+++ source/04_install_extra.py
@@ -8,20 +8,20 @@ import sys, os, shutil
 from moduler.fileOperations import fetch_config
 from moduler.apt_update import apt_update
 from moduler.install_programs import install_programs
+from moduler.download_file import fetch_file
 
 configs = ''
 
 if os.geteuid() != 0:
     sys.exit('Scriptet skal udføres med root access')
 
+filename = '../config/config.ini'
 try:
-    filename = '../config/config.ini'
     configs = fetch_config(filename)
 except Exception as err:
     sys.exit(f'Konfigurationsfilen {filename} kan ikke læses')
 else:
     print(f'Konfigurationsfilen {filename} er indlæst')
-
 
 try:
     apt_update()
@@ -30,20 +30,31 @@ except Exception as err:
 else:
     print('apt-get update og apt-get upgrade udført')
 
-
 try:
     programs = configs['extra.programs']
     options = configs['Common']['install_options']
-    install_programs(programs,options)
+    install_programs(programs, options)
 except Exception as err:
     sys.exit('Der opstod fejl ved installation af ekstra software')
 else:
     print('apt-get installation af ekstra software udført')
 
+version = configs['Common']['docker_compose']
+uri = 'https://github.com/docker/compose/releases/download'
+url = f'{uri}/{version}/docker-compose-Linux-x86_64'
+outfile = '/usr/local/bin/docker-compose'
 try:
-    srcfile = '../config/mongod.conf'
-    dstfile = '/etc/mongod.conf'
-    shutil.copy(srcfile,dstfile)
+    fetch_file(url, outfile)
+    os.chmod(outfile, 0o755)
+except Exception as err:
+    print('Kan ikke installere docker-compose')
+else:
+    print('docker compose er installereet')
+
+srcfile = '../config/mongod.conf'
+dstfile = '/etc/mongod.conf'
+try:
+    shutil.copy(srcfile, dstfile)
 except Exception as err:
     print(err)
     sys.exit(f'Kan ikke kopiere {srcfile} til {dstfile}')
