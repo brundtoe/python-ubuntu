74cc8c3c9a1c44a1dfb9f0400ce2c42e55c3c932
jackie <brundtoe@outlook.dk>
2020-12-18 15:52:09 +0100

    overført url til vagrant

--- ubuntu/vagrant.py
+++ ubuntu/vagrant.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     if os.geteuid() != 0:
        sys.exit('Script skal udføres med root access')
     configs = fetch_config('../config/config.ini')
-    url = configs['vagrantup.com']['url']
+    # https://www.vagrantup.com/downloads.html
     version = configs['Common']['vagrant']
+    url = f"https://releases.hashicorp.com/vagrant/{version}/vagrant_{version}_x86_64.deb"
     print(url, version)
-    install_dpkg(url,version)
+    install_dpkg(url, version)
