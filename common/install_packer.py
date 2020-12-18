59823c40bdadf431ab61c885f4a6a408eeb4ab75
jackie <brundtoe@outlook.dk>
2020-12-18 15:44:12 +0100

    overført url til install_packer.py

--- common/install_packer.py
+++ common/install_packer.py
@@ -4,17 +4,19 @@
 
 from moduler.fileOperations import fetch_config, fetch_archive
 
+
 def install_packer(configs):
     """
     Installation af Packer til opbygning af vagrant bokse med chef/bento
     :param configs: parametre fra config.ini
     :return: void
     """
-    url = configs['packer.io']['url']
-    user = configs['Common']['user']
+    # https://packer.io/
     version = configs['Common']['packer']
+    url = f"https://releases.hashicorp.com/packer/{version}/packer_{version}_linux_amd64.zip"
+    user = configs['Common']['user']
     program = 'Packer'
-    fetch_archive(url,user,program,version,'zip')
+    fetch_archive(url, user, program, version, 'zip')
 
 if __name__ == '__main__':
     configs = fetch_config('../config/config.ini')
