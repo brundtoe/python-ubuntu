447f016613da7587e14a0513ba2287aafbbe10df
jackie <brundtoe@outlook.dk>
2020-12-18 15:33:18 +0100

    overført url til mysqlworkbench

--- common/install_mysql_workbench.py
+++ common/install_mysql_workbench.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     if os.geteuid() != 0:
        sys.exit('Script skal udføres med root access')
     configs = fetch_config('../config/config.ini')
-    url = configs['dev.mysql.com']['url']
     version = configs['Common']['workbench']
+    url = url = f"https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_{version}"
     print(url, version)
-    install_dpkg(url,version)
+    install_dpkg(url, version)
