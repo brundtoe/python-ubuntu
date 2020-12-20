1a7b7dc545e5cbeb279addd06cc4cd87021ea9e2
jackie <brundtoe@outlook.dk>
2020-12-20 17:44:29 +0100

    Download af freeFilesync foretages nu med Curl

--- common/freefilesync.py
+++ common/freefilesync.py
@@ -2,8 +2,14 @@
 #
 # installation af FreeFileSync
 
-from moduler.fileOperations import fetch_config, fetch_archive
+import shutil
+import shlex
+import os
+import subprocess
+
+from moduler.fileOperations import fetch_config
 from moduler.desktopfile import create_desktop_file
+
 
 def install_freefilesync(configs):
     """
@@ -13,13 +19,20 @@ def install_freefilesync(configs):
     """
     # https://freefilesync.org/download.php
     version = configs['Common']['freefilesync']
-    url = f"https://freefilesync.org/download/FreeFileSync_{version}_Linux.tar.gz"
+    filename = f"FreeFileSync_{version}_Linux.tar.gz"
+    url = f"https://freefilesync.org/download/{filename}"
     user = configs['Common']['user']
     program = 'FreeFileSync'
-    fetch_archive(url, user, program, version)
+    down_file = f'/tmp/{filename}'
+    if not os.path.exists(down_file):
+        cmd = shlex.split(f'curl -L -o "{down_file}" {url}')
+        subprocess.run(cmd)
+    unpackedfile = f'/home/{user}/programs'
+    shutil.unpack_archive(f'/tmp/{filename}', unpackedfile)
 
     tmpl = f'{program}.jinja'
     create_desktop_file(program, tmpl, user)
+
 
 if __name__ == '__main__':
     configs = fetch_config('../config/config.ini')
