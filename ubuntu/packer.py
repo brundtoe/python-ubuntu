e9bef82cece707bb53d167fa8cccbb2af3d46dde
jackie <brundtoe@outlook.dk>
2020-12-23 18:17:30 +0100

    ændret packer installation. installation foregår kun når der er en ny version

--- ubuntu/packer.py
+++ ubuntu/packer.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-import sys
 #
 # installation af Packer
+import re
+import shlex
 from os import chown, chmod
 from shutil import move
+from subprocess import run, PIPE
+
 from moduler.fileOperations import fetch_archive
 
 
@@ -17,10 +21,21 @@ def install_packer(configs):
     url = f"https://releases.hashicorp.com/packer/{version}/packer_{version}_linux_amd64.zip"
     user = configs['Common']['user']
     program = 'Packer'
-    fetch_archive(url, user, program, version, 'zip')
+    findes = None
     packer_file = f'/home/{user}/.local/bin/packer'
-    move(f'/home/{user}/programs/packer', packer_file)
-    chown(packer_file, 1000, 1000)
-    chmod(packer_file, 0o755)
-
-
+    try:
+        cmd = shlex.split(f'{packer_file} -v')
+        res = run(cmd, stdout=PIPE, stderr=PIPE)
+    except Exception as err:
+        print(err)
+        print('Packer skal installeres')
+    else:
+        findes = re.search(version, res.stdout.decode('UTF-8'))
+    if findes is None:
+        fetch_archive(url, user, program, version, 'zip')
+        move(f'/home/{user}/programs/packer', packer_file)
+        chown(packer_file, 1000, 1000)
+        chmod(packer_file, 0o755)
+        print('Packer er installeret')
+    else:
+        print('Packer er allerede installeret')
