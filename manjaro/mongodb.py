1e47ac3b05fee89b3edc67404e59a4e8fffb96cc
Jackie <brundtoe@outlook.dk>
2021-01-04 15:13:51 +0100

    mankarp korrektur mongodb

--- manjaro/mongodb.py
+++ manjaro/mongodb.py
@@ -29,36 +29,41 @@ def install_mongodb(configs):
     build_dir = '/tmp/build'
     if not os.path.exists(build_dir):
         os.makedirs(build_dir, 0o755, exist_ok=True)
-    
+
     mongo_bin_dir = f'{build_dir}/mongodb-bin'
-    if not os.path.exists(mongo_bin_dir):
-        os.makedirs(mongo_bin_dir, 0o755, exist_ok=True)
-        cmd = shlex.split(f'git clone https://aur.archlinux.org/mongodb-bin.git {mongo_bin_dir}')
-        res = run(cmd)
-        if res.returncode !=0:
-            print('cloning af mongodb-bin fejlede')
+    old_dir = os.getcwd()
+    try:
+        if not os.path.exists(mongo_bin_dir):
+            os.makedirs(mongo_bin_dir, 0o755, exist_ok=True)
+            cmd = shlex.split(
+                f'git clone https://aur.archlinux.org/mongodb-bin.git {mongo_bin_dir}')
+            run(cmd)
+        os.chdir(mongo_bin_dir)
+        res = run(['makepkg', '-si'])
+    except OSError as err:
+        print(err)
+        print('Installation af MongoDB fra mongodb-bin fejlede')
+    finally:
+        os.chdir(old_dir)
 
+    old_dir = os.getcwd()
     mongo_tools_dir = f'{build_dir}/mongodb-tools-bin'
-    if not os.path.exists(mongo_tools_dir):
-        os.makedirs(mongo_bin_dir, 0o755, exist_ok=True)
-        cmd = shlex.split(f'git clone https://aur.archlinux.org/mongodb-tools-bin.git {mongo_tools_dir}')
-        res = run(cmd)
-        if res.returncode !=0:
-            print('cloning af mongodb-tools-bin fejlede')
-
-    old_dir =os.getcwd()
-    os.chdir(mongo_bin_dir)
-    res = run(['makepkg', '-si'])
-    os.chdir(old_dir)
-
-    old_dir =os.getcwd()
-    os.chdir(mongo_tools_dir)
-    res = run(['makepkg', '-si'])
-    os.chdir(old_dir)
+    try:
+        if not os.path.exists(mongo_tools_dir):
+            os.makedirs(mongo_bin_dir, 0o755, exist_ok=True)
+            cmd = shlex.split(
+                f'git clone https://aur.archlinux.org/mongodb-tools-bin.git {mongo_tools_dir}')
+            run(cmd)
+            os.chdir(mongo_tools_dir)
+            run(['makepkg', '-si'])
+    except OSError as err:
+        print(err)
+        print('Installation af MongoDB fra mongodb-tools-bin fejlede')
+    finally:
+        os.chdir(old_dir)
 
     cmd = shlex.split('sudo systemctl enable mongodb')
-    res = run(cmd)
+    run(cmd)
 
     cmd = shlex.split('sudo systemctl start mongodb')
-    res = run(cmd)
-    
+    run(cmd)
