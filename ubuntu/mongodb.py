61e398ef81e9821575a28aea7140e234bcd96e2f
jackie <brundtoe@outlook.dk>
2021-01-21 18:30:36 +0100

    fejlrettet installation af mongodb debian

--- ubuntu/mongodb.py
+++ ubuntu/mongodb.py
@@ -5,24 +5,32 @@
 
 import sys
 from os import path
+import distro
+import shlex
 from shutil import copyfile
-
+import subprocess
 from moduler.apt_update import apt_update
 from moduler.install_programs import install_program
 from moduler.install_repo import install_repo
 
 
 def install_mongodb(configs):
-    if path.exists('/etc/apt/sources.list.d/mongodb.list'):
-        print('MongoDB er allerede installereet')
+    if path.exists('/usr/lib/systemd/system/mongod.service'):
+        print('MongoDB er allerede installeret')
         return
+    distrib = distro.linux_distribution()[0]
     project_path = configs['Common']['project_path']
     # https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
     mongodb_release = configs['Common']['mongodb_release']
-    lsb_mongodb = configs['Common']['lsb_mongodb']
     repo_key = f"https://www.mongodb.org/static/pgp/server-{mongodb_release}.asc"
-    release = f"{lsb_mongodb}/mongodb-org/{mongodb_release}"
-    sources_string = f"deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu {release} multiverse"
+    if distrib == 'Ubuntu':
+        lsb_mongodb_ubuntu = configs['Common']['lsb_mongodb_ubuntu']
+        release = f"ubuntu {lsb_mongodb_ubuntu}/mongodb-org/{mongodb_release}"
+        sources_string = f"deb [ arch=amd64 ] https://repo.mongodb.org/apt/{release} multiverse"
+    else:
+        lsb_mongodb_debian = configs['Common']['lsb_mongodb_debian']
+        release = f"debian {lsb_mongodb_debian}/mongodb-org/{mongodb_release}"
+        sources_string = f"deb https://repo.mongodb.org/apt/{release} main"
     program = 'mongodb'
     try:
         install_repo(repo_key, program, sources_string)
@@ -36,6 +44,7 @@ def install_mongodb(configs):
     try:
         apt_update()
         install_program('mongodb-org', options)
+        subprocess.run(shlex.split('systemctl stop mongod'))
         print('MongoDB installeret')
         src = f"{project_path}/config/mongod.conf"
         dest = "/etc/mongod.conf"
