0e19dd5212ee44aee15068d27bda83c1096d91d3
jackie <brundtoe@outlook.dk>
2021-01-28 16:56:37 +0100

    ændret archlinux Vagrant installeres kun første gang script runnes.

--- manjaro/vagrant.py
+++ manjaro/vagrant.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 #
 #
-from manjaro.packages import install_program
+from manjaro.packages import install_program, is_installed
 from moduler.vagrant_plugin import vagrant_plugins
 
 
 def install_vagrant(configs):
+
+    if is_installed('vagrant'):
+        print('Vagrant er allerede installeret')
+        return
+
     print('Installation af Vagrant ...')
     try:
         install_program('vagrant')
