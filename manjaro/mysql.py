1bc2aafd39948a69c868f7566d74475d76d4bbe7
jackie <brundtoe@outlook.dk>
2021-01-28 16:51:51 +0100

    ændret archlinux mysql installeres kun første gang script runnes. konfiguration nulstilles hver gang scriptet runnes.

--- manjaro/mysql.py
+++ manjaro/mysql.py
@@ -15,20 +15,21 @@ def install_mysql(configs):
     mysql_daemon = 'mariadb'
     mysql_install_db = shlex.split('mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql')
 
-    if os.path.exists('/usr/lib/systemd/system/mysql.service'):
-        print('MariaDB er allerede installeret')
-    else:
+    if not os.path.exists('/usr/lib/systemd/system/mysql.service'):
         print('Installation af MariaDB')
         try:
             print('Installation af MariaDB')
-            install_program('mariadb')
+            install_program(mysql_daemon)
             run(mysql_install_db)
         except Exception as err:
             print(err)
             sys.exit('Kan ikke installere MariaDB')
+        run(['systemctl', 'enable', mysql_daemon])
+        run(['systemctl', 'start', mysql_daemon])
+    else:
+        print('MariaDB er allerede installeret')
 
-    run(['systemctl', 'enable', mysql_daemon])
-    run(['systemctl', 'start', mysql_daemon])
-
+    print('Konfigurer userer og databaser')
     create_db_users(configs)
+
     print('Afsluttet Installation af MariaDB')
