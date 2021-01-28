7a9ed00f35c5891afb750f330b5574c2ad29f0d0
jackie <brundtoe@outlook.dk>
2021-01-28 16:56:47 +0100

    ændret archlinux Packer installeres kun første gang script runnes.

--- manjaro/packer.py
+++ manjaro/packer.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 #
 #
-from manjaro.packages import install_program
+from manjaro.packages import install_program, is_installed
 
 
 def install_packer(configs):
+
+    if is_installed('packer'):
+        print('Packer er allerede installeret')
+        return
+
     print('Installation af Packer ...')
     try:
         install_program('packer')
